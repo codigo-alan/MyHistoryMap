16bc9b07e3c6396d9e8a18cb7d83c49be2b09dbe
Alan <alanfmarcos@gmail.com>
2023-01-26 18:24:51 +0100

    Creado navigation drawer

---
+++ app/src/main/java/com/example/grafitismap/view/fragments/MarkersListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.grafitismap.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.grafitismap.R
+import com.example.grafitismap.databinding.FragmentMarkersListBinding
+
+
+class MarkersListFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentMarkersListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMarkersListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
