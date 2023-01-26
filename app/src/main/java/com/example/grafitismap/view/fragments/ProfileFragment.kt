6a2977d78e1df239c64dfc34db2fc1307ffb30c8
Alan <alanfmarcos@gmail.com>
2023-01-26 18:54:56 +0100

    Creados todos los fragments, la estructura de la app, y el nav drawer.

---
+++ app/src/main/java/com/example/grafitismap/view/fragments/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.example.grafitismap.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.grafitismap.R
+import com.example.grafitismap.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
