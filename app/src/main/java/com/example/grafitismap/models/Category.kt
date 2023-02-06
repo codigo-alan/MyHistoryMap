90ef3d1361684ec221d0828bb8ae8e523ad585c2
alan.marcos.7e5 <alan.marcos.7e5@itb.cat>
2023-02-06 19:59:01 +0100

    Modificada clase Marker para realm bd. Creada bd, falta crearla en el proyecto y hacer config.

---
+++ app/src/main/java/com/example/grafitismap/models/Category.kt
@@ -0,0 +1,19 @@
+package com.example.grafitismap.models
+
+open class Category {
+}
+
+/**
+ * open class SportRealm(
+@PrimaryKey
+var _id: ObjectId = ObjectId.create(),
+var sport: String = "",
+var hours: Int = 0,
+var owner_id: String = ""
+) : RealmObject {
+// Declaring empty contructor
+constructor() : this(owner_id = "") {}
+//var doAfter: RealmList<Item>? = realmListOf()
+override fun toString() = "SportRealm($_id, $sport, $hours)"
+}
+ */
