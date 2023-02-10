5aa39584b088ef0458c4479155f6823558d3b9de
Alan <alanfmarcos@gmail.com>
2023-02-11 00:47:06 +0100

    Se implementó el uso de cameraX.

--- app/src/main/java/com/example/grafitismap/view/fragments/AddMarkerFragment.kt
+++ app/src/main/java/com/example/grafitismap/view/fragments/AddMarkerFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.grafitismap.R
 import com.example.grafitismap.databinding.FragmentAddMarkerBinding
 
@@ -24,6 +25,10 @@ class AddMarkerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.takePhtoBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_addMarkerFragment_to_cameraFragment)
+        }
     }
 
 }
