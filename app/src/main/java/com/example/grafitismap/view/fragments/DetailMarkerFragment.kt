fdc253bc0624ae88eac5a4cdf3db4061d5d65d07
Alan <alanfmarcos@gmail.com>
2023-03-08 21:20:23 +0100

    MarkersListFragment.kt utiliza GrafitisViewModel.kt como view model. Se ve bien el recycler ahora.

--- app/src/main/java/com/example/grafitismap/view/fragments/DetailMarkerFragment.kt
+++ app/src/main/java/com/example/grafitismap/view/fragments/DetailMarkerFragment.kt
@@ -9,12 +9,12 @@ import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
 import com.example.grafitismap.databinding.FragmentDetailMarkerBinding
 import com.example.grafitismap.viewmodel.GrafitisViewModel
-import com.example.grafitismap.viewmodel.MarkersListViewModel
+
 
 class DetailMarkerFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailMarkerBinding
-    private val viewModel: MarkersListViewModel by activityViewModels()
+    private val viewModel: GrafitisViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
